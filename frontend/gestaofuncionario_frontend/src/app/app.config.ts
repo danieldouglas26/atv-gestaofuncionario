8bb93e9f97865c385d027b6e032964306e3dff95
danieldouglas261 <danieldouglas26@outlook.com>
2025-10-27 20:25:59 -0300

    Feature: Atualizando APP config

--- frontend/gestaofuncionario_frontend/src/app/app.config.ts
+++ frontend/gestaofuncionario_frontend/src/app/app.config.ts
@@ -1,11 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations'; // Importar
+import { ConfirmationService, MessageService } from 'primeng/api'; // Importar
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(withFetch()),
+    provideAnimations(),
+    MessageService,
+    ConfirmationService
   ]
 };
