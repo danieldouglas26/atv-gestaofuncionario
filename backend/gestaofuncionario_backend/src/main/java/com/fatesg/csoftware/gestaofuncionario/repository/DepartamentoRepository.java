8201cabc029f45f6217cb45db674e38dc9a5a7b0
Daniel Douglas <161387116+danieldouglas26@users.noreply.github.com>
2025-10-27 20:07:38 -0300

    Feature : Criação do Repository para o Departamento

--- backend/gestaofuncionario_backend/src/main/java/com/fatesg/csoftware/gestaofuncionario/repository/DepartamentoRepository.java
+++ backend/gestaofuncionario_backend/src/main/java/com/fatesg/csoftware/gestaofuncionario/repository/DepartamentoRepository.java
@@ -1,7 +1,7 @@
 package com.fatesg.csoftware.gestaofuncionario.repository;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Optional; 
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
