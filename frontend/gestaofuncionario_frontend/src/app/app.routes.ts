c977398e2678166944cc32413ed0f7231e9abb69
danieldouglas261 <danieldouglas26@outlook.com>
2025-10-27 20:25:03 -0300

    Refactor: Atualizando o APP routes para as novas rotas do Departamento

--- frontend/gestaofuncionario_frontend/src/app/app.routes.ts
+++ frontend/gestaofuncionario_frontend/src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
 import { FuncionarioListComponent } from './components/funcionario-list/funcionario-list.component';
 import { FuncionarioFormComponent } from './components/funcionario-form/funcionario-form.component';
+import { DepartamentoListComponent } from './components/departamento-list/departamento-list.component';
+import { DepartamentoFormComponent } from './components/departamento-form/departamento-form.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'funcionarios', pathMatch: 'full' },
   { path: 'funcionarios', component: FuncionarioListComponent },
   { path: 'funcionarios/novo', component: FuncionarioFormComponent },
-  { path: 'funcionarios/editar/:id', component: FuncionarioFormComponent }
+  { path: 'funcionarios/editar/:id', component: FuncionarioFormComponent },
+
+  { path: 'departamentos', component: DepartamentoListComponent },
+  { path: 'departamentos/novo', component: DepartamentoFormComponent },
+  { path: 'departamentos/editar/:id', component: DepartamentoFormComponent },
+
+  { path: '**', redirectTo: 'funcionarios' }
 ];
