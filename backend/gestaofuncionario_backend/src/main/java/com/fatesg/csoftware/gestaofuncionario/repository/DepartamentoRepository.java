1376c50248f01d0bcfb538954912a5ed87e9ea58
danieldouglas261 <danieldouglas26@outlook.com>
2025-10-27 19:49:21 -0300

    Novo repository para o departamento

---
+++ backend/gestaofuncionario_backend/src/main/java/com/fatesg/csoftware/gestaofuncionario/repository/DepartamentoRepository.java
@@ -0,0 +1,22 @@
+package com.fatesg.csoftware.gestaofuncionario.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fatesg.csoftware.gestaofuncionario.model.Departamento;
+
+@Repository
+public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
+
+    boolean existsByNome(String nome);
+
+    boolean existsByNomeAndIdNot(String nome, Long id);
+
+    List<Departamento> findByAtivoTrue();
+
+    Optional<Departamento> findByNome(String nome);
+
+}
