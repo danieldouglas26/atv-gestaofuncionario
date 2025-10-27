20734e6725fc710b9bf1f683e98f43277fc5bcb8
danieldouglas261 <danieldouglas26@outlook.com>
2025-10-27 20:20:37 -0300

    Feature: Criação do service do departamento

---
+++ frontend/gestaofuncionario_frontend/src/app/services/departamento.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Departamento } from '../models/departamento.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DepartamentoService {
+
+  private baseUrl = 'http://localhost:8080/api/departamentos';
+
+  constructor(private http: HttpClient) { }
+
+  listar(): Observable<Departamento[]> {
+    return this.http.get<Departamento[]>(this.baseUrl);
+  }
+
+  listarAtivos(): Observable<Departamento[]> {
+    return this.http.get<Departamento[]>(`${this.baseUrl}/ativos`);
+  }
+
+  buscarPorId(id: number): Observable<Departamento> {
+    return this.http.get<Departamento>(`${this.baseUrl}/${id}`);
+  }
+
+  criar(departamento: { nome: string; sigla: string }): Observable<Departamento> {
+    return this.http.post<Departamento>(this.baseUrl, departamento);
+  }
+
+  atualizar(id: number, departamento: { nome: string; sigla: string }): Observable<Departamento> {
+    return this.http.put<Departamento>(`${this.baseUrl}/${id}`, departamento);
+  }
+
+  inativar(id: number): Observable<void> {
+    return this.http.patch<void>(`${this.baseUrl}/${id}/inativar`, {});
+  }
+}
