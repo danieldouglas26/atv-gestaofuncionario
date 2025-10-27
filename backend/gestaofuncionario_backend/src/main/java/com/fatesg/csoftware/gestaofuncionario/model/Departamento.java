daa366442acac94dc6745f72460ad66c76cd0899
Daniel Douglas <161387116+danieldouglas26@users.noreply.github.com>
2025-10-27 20:08:39 -0300

    Feature: Criação da nova entidade

--- backend/gestaofuncionario_backend/src/main/java/com/fatesg/csoftware/gestaofuncionario/model/Departamento.java
+++ backend/gestaofuncionario_backend/src/main/java/com/fatesg/csoftware/gestaofuncionario/model/Departamento.java
@@ -2,7 +2,7 @@ package com.fatesg.csoftware.gestaofuncionario.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+import java.util.Objects; 
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
