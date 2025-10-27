0e23e40ca47099d18a55264d4a5f006b256025ba
Daniel Douglas <161387116+danieldouglas26@users.noreply.github.com>
2025-10-27 20:10:04 -0300

    Feature: 	 Criação do Controller do departamento

--- backend/gestaofuncionario_backend/src/main/java/com/fatesg/csoftware/gestaofuncionario/controller/DepartamentoController.java
+++ backend/gestaofuncionario_backend/src/main/java/com/fatesg/csoftware/gestaofuncionario/controller/DepartamentoController.java
@@ -1,7 +1,7 @@
 package com.fatesg.csoftware.gestaofuncionario.controller;
 
 import java.util.List;
-
+ 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
