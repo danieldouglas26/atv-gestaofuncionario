96ecc319c94e0adcee489ceee9d325c3f30b28b5
danieldouglas261 <danieldouglas26@outlook.com>
2025-10-27 20:30:44 -0300

    Fix: Atualizando funcionário model para os novos campos do backend

--- frontend/gestaofuncionario_frontend/src/app/models/funcionario.model.ts
+++ frontend/gestaofuncionario_frontend/src/app/models/funcionario.model.ts
@@ -12,6 +12,12 @@ export interface Funcionario {
   email: string;
   cargo: string;
   salario: number;
-  dataAdmissao: string;
+  dataAdmissao: string; // ou Date, dependendo da sua preferência de manipulação
   ativo: boolean;
+
+  // Campos do Departamento (Regra 3.4)
+  departamentoId: number;
+  departamentoNome: string;
+  departamentoSigla: string;
+  departamentoAtivo: boolean; // (Regra 3.2 - Aviso)
 }
