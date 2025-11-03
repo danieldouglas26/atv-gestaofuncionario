cc5fc111e80ee0f5aa271f70b3ede14c9284b28e
danieldouglas261 <danieldouglas26@outlook.com>
2025-11-03 15:05:25 -0300

    Fix: Corrigindo bugs do app Componente

--- frontend/gestaofuncionario_frontend/src/app/app.component.ts
+++ frontend/gestaofuncionario_frontend/src/app/app.component.ts
@@ -1,33 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
 import { AuthService } from './services/auth.service';
+
 import { ToastModule } from 'primeng/toast';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-
   imports: [
     CommonModule,
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
     ToastModule,
-    ConfirmDialogModule
+    ConfirmDialogModule,
+    ButtonModule
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gestao-de-pessoal';
 
   isAuthenticated$: Observable<boolean>;
+  userEmail: string | null = null;
 
   constructor(private authService: AuthService) {
-
     this.isAuthenticated$ = this.authService.authState$;
+  }
+
+
+  ngOnInit(): void {
+
+    this.authService.authState$.subscribe(isAuth => {
+      if (isAuth) {
+        this.userEmail = this.authService.getUserEmail();
+      } else {
+        this.userEmail = null;
+      }
+    });
+  }
+
+
+  logout(): void {
+    this.authService.logout();
   }
 }
