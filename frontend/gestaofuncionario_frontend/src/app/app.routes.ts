29409fc8873d6757fce6f147930a7606800d7bcc
danieldouglas261 <danieldouglas26@outlook.com>
2025-11-03 14:29:46 -0300

    Refactor: Modificando o APP routes para incluir as rotas protegidas

--- frontend/gestaofuncionario_frontend/src/app/app.routes.ts
+++ frontend/gestaofuncionario_frontend/src/app/app.routes.ts
@@ -1,18 +1,53 @@
 import { Routes } from '@angular/router';
-import { FuncionarioListComponent } from './components/funcionario-list/funcionario-list.component';
-import { FuncionarioFormComponent } from './components/funcionario-form/funcionario-form.component';
+import { authGuard } from './guards/auth.guard';
+import { unsavedChangesGuard } from './guards/unsaved-changes.guard';
+import { LoginComponent } from './login/login.component';
 import { DepartamentoListComponent } from './components/departamento-list/departamento-list.component';
 import { DepartamentoFormComponent } from './components/departamento-form/departamento-form.component';
+import { FuncionarioListComponent } from './components/funcionario-list/funcionario-list.component';
+import { FuncionarioFormComponent } from './components/funcionario-form/funcionario-form.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'funcionarios', pathMatch: 'full' },
-  { path: 'funcionarios', component: FuncionarioListComponent },
-  { path: 'funcionarios/novo', component: FuncionarioFormComponent },
-  { path: 'funcionarios/editar/:id', component: FuncionarioFormComponent },
 
-  { path: 'departamentos', component: DepartamentoListComponent },
-  { path: 'departamentos/novo', component: DepartamentoFormComponent },
-  { path: 'departamentos/editar/:id', component: DepartamentoFormComponent },
+  { path: 'login', component: LoginComponent },
 
-  { path: '**', redirectTo: 'funcionarios' }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  {
+    path: 'departamentos',
+    component: DepartamentoListComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'departamentos/novo',
+    component: DepartamentoFormComponent,
+    canActivate: [authGuard],
+    canDeactivate: [unsavedChangesGuard]
+  },
+  {
+    path: 'departamentos/:id',
+    component: DepartamentoFormComponent,
+    canActivate: [authGuard],
+    canDeactivate: [unsavedChangesGuard]
+  },
+
+  {
+    path: 'funcionarios',
+    component: FuncionarioListComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'funcionarios/novo',
+    component: FuncionarioFormComponent,
+    canActivate: [authGuard],
+    canDeactivate: [unsavedChangesGuard]
+  },
+  {
+    path: 'funcionarios/:id',
+    component: FuncionarioFormComponent,
+    canActivate: [authGuard],
+    canDeactivate: [unsavedChangesGuard]
+  },
+
+  { path: '**', redirectTo: 'login' }
 ];
