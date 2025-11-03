d4695e47c7cefdb4d6ea61e00a5afc1409753697
danieldouglas261 <danieldouglas26@outlook.com>
2025-11-03 14:40:13 -0300

    Fix: Corrijindo bug do app componente

--- frontend/gestaofuncionario_frontend/src/app/app.component.ts
+++ frontend/gestaofuncionario_frontend/src/app/app.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
-import { HeaderComponent } from './header/header.component';
 import { AuthService } from './services/auth.service';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 
 @Component({
   selector: 'app-root',
   standalone: true,
 
-  imports: [RouterOutlet, CommonModule, HeaderComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    ToastModule,
+    ConfirmDialogModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'gestao-funcionarios-frontend';
+  title = 'gestao-de-pessoal';
 
   isAuthenticated$: Observable<boolean>;
 
